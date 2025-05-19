2e6bfa0f921991524f3b1c5feb19ddfff64c1f2d
rutujakirkade <rutujakirkade09@gmail.com>
2025-05-20 00:31:32 +0530

    for loop logic
    for loop logic

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,15 @@
+ 
 
 
+ console.log("Stew Jobs");
 
-console.log("-------------Hello World-------------");
+ var fruit; // Variable Declaration
+ console.log("fruit variable declaration:", fruit);
+ fruit = "Apple"; // Variable Initialization
+ console.log("fruit variable Initialization: ", fruit);
+ 
+ fruit = "Orange"; // Variable Update
+ console.log("fruit updated value: ", fruit);
+ 
+ fruit = "Kiwi"; // Variable Update
+ console.log("fruit updated value: ", fruit);
